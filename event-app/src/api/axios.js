b1880f307485d9da1f7f791bb01e665d6b6f929a
kieron <koteykieron5@gmail.com>
2024-12-16 23:56:27 +0000

    Add React DevTools disabling for production

--- event-app/src/api/axios.js
+++ event-app/src/api/axios.js
@@ -2,7 +2,11 @@ import axios from 'axios';
 
 const api = axios.create({
     baseURL: 'https://campus-project-ljun.onrender.com',
-    withCredentials: true
+    withCredentials: true,
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
 });
 
 export default api; 
--- event-app/src/index.js
+++ event-app/src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App.jsx';
-import reportWebVitals from './reportWebVitals';
-import {
-  createBrowserRouter,RouterProvider,
-  Router,
-}from "react-router-dom"
+import App from './App';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 
-
-
+// Disable React DevTools in production
+if (process.env.NODE_ENV === 'production') {
+    disableReactDevTools();
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -17,8 +15,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
